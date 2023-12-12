18d1e2562d4aa7536e491d0fafd56dcd5feb4680
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-12 20:39:01 +0700

    Заготовка для анимаций

--- app/src/main/java/com/elmirov/animationscompose/MainActivity.kt
+++ app/src/main/java/com/elmirov/animationscompose/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.elmirov.animationscompose.ui.screen.Test
 import com.elmirov.animationscompose.ui.theme.AnimationsComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +15,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AnimationsComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Test()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AnimationsComposeTheme {
-        Greeting("Android")
     }
 }
