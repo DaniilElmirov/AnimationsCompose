1698d45b906df7168e642ef50cba56321e41bcbf
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-12 23:25:07 +0700

    Заготовка для анимаций контента

--- app/src/main/java/com/elmirov/animationscompose/MainActivity.kt
+++ app/src/main/java/com/elmirov/animationscompose/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.elmirov.animationscompose.ui.screen.Test
+import com.elmirov.animationscompose.ui.screen.AnimateContent
 import com.elmirov.animationscompose.ui.theme.AnimationsComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +19,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Test()
+                    //Test()
+                    AnimateContent()
                 }
             }
         }
